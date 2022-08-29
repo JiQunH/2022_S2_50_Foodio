a81254dbaf5cf767bdc0a6b59444f91d4631be92
JiQunHuang <jiqun.huang@gmail.com>
2022-08-29 15:28:36 +1200

    Restaurant image view created

--- app/src/main/java/com/example/foodio/RestaurantsAdaper.kt
+++ app/src/main/java/com/example/foodio/RestaurantsAdaper.kt
@@ -1,15 +1,26 @@
 package com.example.foodio
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 
 class RestaurantsAdapter(val context : Context, val restaurants: List<YelpRestaurant>) :
     RecyclerView.Adapter<RestaurantsAdapter.ViewHolder>() {
+
+
+    fun addImages(){
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -28,7 +39,12 @@ class RestaurantsAdapter(val context : Context, val restaurants: List<YelpRestau
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(restaurant: YelpRestaurant) {
           val tvName = itemView.findViewById<TextView>(R.id.tvName)
+            val image = itemView.findViewById<ImageView>(R.id.imageView)
             tvName.text = restaurant.name
+            Glide.with(context).load(restaurant.imageUrl).apply(
+                RequestOptions().transform(
+                CenterCrop(), RoundedCorners(20)
+            )).into(image)
         }
     }
 }
