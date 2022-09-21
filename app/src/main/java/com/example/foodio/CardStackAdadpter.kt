597ac5b5617cc18edd663ebb667de7f00785c70a
LegendKeyur01 <keyurnaidu01@gmail.com>
2022-09-21 13:52:03 +1200

    Comments on StackAdapter

--- app/src/main/java/com/example/foodio/CardStackAdadpter.kt
+++ app/src/main/java/com/example/foodio/CardStackAdadpter.kt
@@ -17,12 +17,14 @@ class CardStackAdadpter(val context: Context, val restaurants : List<YelpRestaur
     inner class CardViewHolder(val binding : CardRestaurantBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(restaurant: YelpRestaurant){
             binding.apply {
+               //Gets variable from Dataclass end points ands and sets it to the UI
                 tvRName.text = restaurant.name
                 tvRAddress.text = restaurant.location.address
                 tvRPhone.text = restaurant.phone
                 RRating.rating = restaurant.rating!!.toFloat()
                 tvRPrice.text = restaurant.price
                 tvRPhone.text = restaurant.phone
+                //Allows for image to be displayed of type context
                 Glide.with(context).load(restaurant.imageUrl).apply(
                     RequestOptions().transform(
                         CenterCrop(), RoundedCorners(20)
@@ -32,17 +34,21 @@ class CardStackAdadpter(val context: Context, val restaurants : List<YelpRestaur
         }
     }
 
+    //Holds layout of indivdual item in the list
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
         cardBinding = CardRestaurantBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return CardViewHolder(cardBinding)
     }
 
+    //Display data at a certain position and update
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         holder.bind(restaurants[position])
     }
 
+    //Gets Size of Restaurant into method
     override fun getItemCount() = restaurants.size
 
+    //Retrn the restaurant at Position number parsed
     fun returnRestaurant(position: Int) : YelpRestaurant{
         return restaurants[position]
     }
