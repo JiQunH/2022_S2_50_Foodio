42a6428534ece66abf352379c9a5ed2dc62d4dc4
LegendKeyur01 <keyurnaidu01@gmail.com>
2022-10-04 11:15:05 +1300

    Added Fragment Setting XML & Setting Fragment

--- app/src/main/java/com/example/foodio/MainActivity.kt
+++ app/src/main/java/com/example/foodio/MainActivity.kt
@@ -24,6 +24,11 @@ class MainActivity : AppCompatActivity(){
                 R.id.restaurants ->{
                     loadFragment(RestaurantsFragment())
                 }
+                //TODO
+                R.id.settings ->{
+                    loadFragment(SettingFragment())
+                }
+
             }
         }
     }
